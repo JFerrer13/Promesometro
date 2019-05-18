bc1cf8db799ad7484aec289990b54c1151fcfbec
Javier Ferrer <Javi@iMac-de-Morgan.local>
2019-05-17 21:30:08 -0600

    Primer confimación.

---
+++ script/scripts.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    Frameworks: [
+      {
+        icono: 'fab fa-vuejs',
+        color: '#42b983',
+        nombre:'Vue',
+      },
+      {
+        icono: 'fab fa-font-awesome-flag',
+        color: '#298be6',
+        nombre:'FontAwesome',
+      },
+      {
+        icono: 'fab fa-bootstrap',
+        color: '#563d7c',
+        nombre:'Bootstrap',
+      },
+    ]
+  },
+  methods:{
+  }
+})
