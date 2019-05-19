f2f8e685398cdf14fac61c58143c7491cdf91e6d
Javier Ferrer <javierferrer@galileo.edu>
2019-05-19 11:05:39 -0600

    Agrego los tuits feos pero ahi estan

--- components/tuit-pull.js
+++ components/tuit-pull.js
@@ -6,9 +6,10 @@ Vue.component('tuit-pull', {
           <div class="col-12 text-center"><h3>Los tweets</h3></div>
         </div>
         <div class="row">
-        <div class="row">
-          <div v-for="item in tweets" >
-            <tuit usuario="tiem.usuario"></tuit> </div>
+            <div class="row">
+                <div v-for="item in tweets" >
+                    <tuit :usuario="item.usuario" :fecha="item.fecha" :tuitt="item.tuit" :tuit="item.tuit" :porcentaje="item.porcentaje"></tuit> 
+                </div>
             </div> 
         </div>      
     </section>
@@ -22,18 +23,20 @@ Vue.component('tuit-pull', {
     },  
     methods:{
         consulta(){                    
-            const url = "obtenertuits?tuser=panresponde&partido=GT_CREO";
+            const url = "obtenertuits?tuser=creoguate&partido=GT_CREO";
             axios.get(this.Url + url).then(response => {
                 if(response.status == 200){                    
                     let datos = JSON.parse(response.data.body);
-                    
+                    console.log(datos);
                     for(var i = 0; i < datos.length; i++ ){
                         this.tweets.push({
                             usuario: datos[i][0],
                             fecha: datos[i][2],
+                            tuit: datos[i][3],
+                            porcentaje: Math.trunc(datos[i][5]),
                         })
                     } 
-                    
+                    console.log(this.tweets);
                 }                
             }).catch(err =>  {
                 //alert("Algo salio mal");
--- components/tuit.js
+++ components/tuit.js
@@ -6,19 +6,17 @@ Vue.component('tuit', {
             <div class="row">
                 <div class="col-md-9">
                     <div class="row">
-                        <div class="col-md-12">
-                            <button type="button" class="btn btn-outline-secondary" v-for="item in etiquetas" v-text="item"></button>
-                        </div>
+
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <h5>
+                            <h3>
                                 <span class="badge badge-secondary" v-text="porcentaje"></span>
                                 <strong v-text="'@' + usuario"></strong>
-                                <span class="text-muted" v-text="data-fecha"></strong>
-                            </h4>
-                            <p class="text-muted" v-text="data-tuit"></p>
-                            <a class="btn btn-info" :href="href" >ver en <i class="fab fa-twitter"></i></a>
+                                <span class="text-muted" v-text="fecha"></strong>
+                            </h3>
+                            <p class="text-muted" v-text="tuitt"></p>
+                            <a class="btn btn-info" style="color:white" :href="href" >ver en <i class="fab fa-twitter"></i></a>
                         </div>
                     </div>
                 </div>
@@ -26,5 +24,5 @@ Vue.component('tuit', {
         </div>
     </div>
     `,
-    props: ['usuario', 'data-fecha', 'data-tuit', 'href', 'porcentaje'],
+    props: ['usuario', 'fecha', 'tuitt', 'href', 'porcentaje'],
 });
